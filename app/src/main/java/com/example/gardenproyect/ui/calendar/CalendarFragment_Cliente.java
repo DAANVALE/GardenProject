7247b44c3865ccb9ade0462eeb54b99c6badf0fa
DAANVALE <a17300435@ceti.mx>
2021-03-22 16:24:18 -0600

    v0.6.1

--- app/src/main/java/com/example/gardenproyect/ui/calendar/CalendarFragment_Cliente.java
+++ app/src/main/java/com/example/gardenproyect/ui/calendar/CalendarFragment_Cliente.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.example.gardenproyect.R;
 import com.example.gardenproyect.ui.service.ServiceViewModel_Cliente;
@@ -27,5 +29,19 @@ public class CalendarFragment_Cliente extends Fragment {
                 ViewModelProviders.of(this).get(CalendarViewModel_Cliente.class);
         View root = inflater.inflate(R.layout.fragment_calendar_cliente, container, false);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnTime = view.findViewById(R.id.btnTime);
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.nav_time_cliente);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/gardenproyect/ui/service/ServiceFragment_Cliente.java
+++ app/src/main/java/com/example/gardenproyect/ui/service/ServiceFragment_Cliente.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.example.gardenproyect.R;
 import com.example.gardenproyect.ui.home.HomeViewModel_Cliente;
@@ -25,5 +27,18 @@ public class ServiceFragment_Cliente extends Fragment {
                 ViewModelProviders.of(this).get(ServiceViewModel_Cliente.class);
         View root = inflater.inflate(R.layout.fragment_service_cliente, container, false);
         return root;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnCalendar = view.findViewById(R.id.btnCalendar);
+
+        btnCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.nav_calendar_cliente);
+            }
+        });
     }
 }
