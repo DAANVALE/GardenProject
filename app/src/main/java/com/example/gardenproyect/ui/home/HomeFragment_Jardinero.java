39c4b884efe25ce733cffac70ab1affba7fd391f
GiovanniVegaTorres <a17300437@ceti.mx>
2021-03-22 17:02:02 -0600

    Nueva

--- app/src/main/java/com/example/gardenproyect/ui/home/HomeFragment_Jardinero.java
+++ app/src/main/java/com/example/gardenproyect/ui/home/HomeFragment_Jardinero.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.example.gardenproyect.R;
 
@@ -25,6 +27,20 @@ public class HomeFragment_Jardinero extends Fragment{
                 ViewModelProviders.of((Fragment) this).get(HomeViewModel_Jardinero.class);
         View root = inflater.inflate(R.layout.fragment_home_jardinero, container, false);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnAgendar = view.findViewById(R.id.btnAgendar);
+
+        btnAgendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.nav_service_Jardinero);
+            }
+        });
     }
 }
 
