3b9275ab2716c47c320de9682e3999675a8e8e2a
GiovanniVegaTorres <a17300437@ceti.mx>
2021-03-30 16:46:04 -0600

    version 0.6.3.1

--- app/src/main/java/com/example/gardenproyect/ui/time/TimeFragment_Cliente.java
+++ app/src/main/java/com/example/gardenproyect/ui/time/TimeFragment_Cliente.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.example.gardenproyect.R;
 import com.example.gardenproyect.ui.service.ServiceViewModel_Cliente;
@@ -25,5 +27,19 @@ public class TimeFragment_Cliente extends Fragment {
                 ViewModelProviders.of(this).get(TimeViewModel_Cliente.class);
         View root = inflater.inflate(R.layout.fragment_time_cliente, container, false);
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnHora = view.findViewById(R.id.btnHora);
+
+        btnHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.nav_service_cliente);
+            }
+        });
     }
 }
