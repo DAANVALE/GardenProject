2733cb6eb84dee1be47a83dede6671e971b6f995
GiovanniVegaTorres <a17300437@ceti.mx>
2021-03-31 19:25:03 -0600

    Version 0.6.3.5

--- app/src/main/java/com/example/gardenproyect/ui/service/ServiceFragment_Cliente.java
+++ app/src/main/java/com/example/gardenproyect/ui/service/ServiceFragment_Cliente.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,9 +18,11 @@ import androidx.navigation.Navigation;
 import com.example.gardenproyect.R;
 import com.example.gardenproyect.ui.home.HomeViewModel_Cliente;
 
-public class ServiceFragment_Cliente extends Fragment {
+public class ServiceFragment_Cliente extends Fragment implements View.OnClickListener {
 
     private ServiceViewModel_Cliente ServiceViewModelCliente;
+    Button btnDate, btnHour;
+    EditText TxtVDate, TxtVHour;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,20 +30,33 @@ public class ServiceFragment_Cliente extends Fragment {
                 ViewModelProviders.of(this).get(ServiceViewModel_Cliente.class);
         View root = inflater.inflate(R.layout.fragment_service_cliente, container, false);
         return root;
+
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        btnDate = (Button) btnDate.findViewById(R.id.btnDate);
+        btnHour = (Button) btnHour.findViewById(R.id.btnHour);
+        TxtVDate = (EditText) TxtVDate.findViewById(R.id.EdTDate);
+        TxtVHour = (EditText) TxtVHour.findViewById(R.id.EdTHour);
+
+        btnDate.setOnClickListener(this);
+        btnHour.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Button btnCalendar = view.findViewById(R.id.btnCalendar);
         Button btnTypeService = view.findViewById(R.id.btnAgregarTipoServicio);
-
-        btnCalendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.nav_calendar_cliente);
-            }
-        });
 
         btnTypeService.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,4 +66,5 @@ public class ServiceFragment_Cliente extends Fragment {
         });
 
     }
+
 }
