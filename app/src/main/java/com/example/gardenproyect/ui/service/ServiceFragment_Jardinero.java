3a329569a6823f6c5071b76491e574a29c672b4a
GiovanniVegaTorres <a17300437@ceti.mx>
2021-05-13 19:41:46 -0500

    version 0.8.7

--- app/src/main/java/com/example/gardenproyect/ui/service/ServiceFragment_Jardinero.java
+++ app/src/main/java/com/example/gardenproyect/ui/service/ServiceFragment_Jardinero.java
@@ -1,5 +1,7 @@
 package com.example.gardenproyect.ui.service;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -15,20 +18,33 @@ import androidx.navigation.Navigation;
 
 import com.example.gardenproyect.R;
 import com.example.gardenproyect.ui.home.HomeViewModel_Cliente;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class ServiceFragment_Jardinero extends Fragment {
+public class ServiceFragment_Jardinero extends Fragment implements OnMapReadyCallback {
 
     private ServiceViewModel_Jardinero ServiceViewModelJardinero;
 
     MaterialButton logoutButton;
 
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View root;
+    private static final int LOCATION_REQUEST = 500;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ServiceViewModelJardinero =
                 ViewModelProviders.of(this).get(ServiceViewModel_Jardinero.class);
-        View root = inflater.inflate(R.layout.fragment_service_jardinero, container, false);
+        root = inflater.inflate(R.layout.fragment_service_jardinero, container, false);
         return root;
     }
 
@@ -37,6 +53,7 @@ public class ServiceFragment_Jardinero extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         logoutButton = view.findViewById(R.id.logoutButton);
+        mMapView = (MapView) root.findViewById(R.id.map);
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,5 +63,45 @@ public class ServiceFragment_Jardinero extends Fragment {
             }
         });
 
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mGoogleMap.setMyLocationEnabled(true);
+
+        marcadores(googleMap);
+    }
+
+    private void marcadores(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(20.6648312,-103.2086447)).title("Casa").snippet("Aqui haciendo el proyecto"));
+
+        CameraPosition Casa = CameraPosition.builder().target(new LatLng(20.6648312,-103.2086447)).zoom(16).bearing(0).tilt(45).build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Casa));
+    }
+
 }
